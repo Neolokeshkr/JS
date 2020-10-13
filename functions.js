3be70b3a465519b1a8d28a673623569da8a84abe
neolokeshkr <neolokeshkr@gmail.com>
2020-10-13 23:24:45 +0530

    This is the JS repo

---
+++ functions.js
@@ -0,0 +1,21 @@
+console.log("Hello world");
+function outside(){
+	let a = 10;
+	var b = 15;
+
+
+	function inside(){
+		let c = 20;
+		let d = 45;
+	//	return [console.log(a),console.log(b)]
+		console.log(a);
+		console.log(b);
+		}
+
+	inside();
+	
+	//	console.log(c);
+	//	console.log(d);
+}
+outside();	
+
