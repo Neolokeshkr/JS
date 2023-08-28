f661e8887c347a6fcd3cfe9684f741c62b197bd7
Lokesh Kumar <neolokeshkr@gmail.com>
2023-08-28 16:18:08 +0530

    Program to print random number between any range and without using Math object added

---
+++ Coding Questions/randomNumberWithoutMath.js
@@ -0,0 +1,10 @@
+//  Program to print a random number between a range without using Math Object
+function printRandomNumer(min, max){
+    let timeStamp = Date.now();
+    let range = max - min  + 1;
+    let randomNumber = timeStamp%range + 1;
+
+    return randomNumber;
+}
+
+ console.log(printRandomNumer(1,89));
